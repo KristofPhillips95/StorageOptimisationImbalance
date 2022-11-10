df9f1110df9e2bd3bc424caaf0c0351e2aab6426
Kristof Phillips <Kristof.phillips@kuleuven.be>
2022-11-10 19:14:58 +0100

    An initial script to start the implementation/testing of frequent script running on AWS.

--- Scripts/TimeScheduler.py
+++ Scripts/TimeScheduler.py
@@ -1,12 +1,21 @@
-#!/usr/bin/env python3
-import sched, time
-import TimeWriter
-s = sched.scheduler(time.time, time.sleep)
-def do_something(sc): 
-    print("Writing Time...")
-    # do your stuff
-    TimeWriter.write_time()
-    sc.enter(10, 1, do_something, (sc,))
+# !/usr/bin/env python3
+from datetime import datetime
+import pandas as pd
+import os
 
-s.enter(10, 1, do_something, (s,))
-s.run()
+
+# print("script running")
+def write_time():
+    filename = os.path.join(os.getcwd(), "time_file.csv")
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    row = pd.DataFrame(data=[current_time], columns=["Timestamp"])
+
+    if os.path.isfile(filename):
+        df_times = pd.read_csv(filename, index_col=0)
+        df_times = df_times.append(row, ignore_index=True)
+    else:
+        df_times = row
+    # print("Time to write to file")
+    # print(filename)
+    df_times.to_csv(filename)
