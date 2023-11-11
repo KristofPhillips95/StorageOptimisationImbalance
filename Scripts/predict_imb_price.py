3bd3c8a3721f21f8eede46ae1609ceeddaed155e
Ruben Smets <ruben.smets1@kuleuven.be>
2023-11-11 18:29:45 -0500

    pred_SI function

--- Scripts/predict_imb_price.py
+++ Scripts/predict_imb_price.py
@@ -66,8 +66,7 @@ def load_forecaster(dict,type):
 
     return model
 
-
-if __name__ == '__main__':
+def pred_SI():
 
     la = 10
     store_code = "20231103_test"
@@ -78,49 +77,51 @@ if __name__ == '__main__':
     with open(path_data, 'rb') as file:
         dict_datapoints = pickle.load(file)
 
-
     dict_pred = {
         'lookahead': 10,
         'lookback': 4,
-        #'data_past': ['RT_load','DA_F_load','RT_wind','DA_F_wind','RT_pv','DA_F_pv','RT_SI'],
-        #'data_fut': ['DA_F_load','DA_F_wind','DA_F_pv','DA_F_nuclear','DA_F_gas'],
+        # 'data_past': ['RT_load','DA_F_load','RT_wind','DA_F_wind','RT_pv','DA_F_pv','RT_SI'],
+        # 'data_fut': ['DA_F_load','DA_F_wind','DA_F_pv','DA_F_nuclear','DA_F_gas'],
         'data_past': dict_datapoints['read_cols_past_ctxt'],
         'data_fut': dict_datapoints['read_cols_fut_ctxt'],
         'cols_temp': dict_datapoints['cols_temp'],
-        #'loc_SI_FC': 'train_SI_forecaster/output/trained_models/LA_10/20230503/config_3.pt',
+        # 'loc_SI_FC': 'train_SI_forecaster/output/trained_models/LA_10/20230503/config_3.pt',
         'loc_SI_FC': 'train_SI_forecaster/output/trained_models/LA_10/20231103_test/config_1.pt',
         'loc_price_FC': ''
     }
 
-
-    si_forecaster = load_forecaster(dict=dict_pred,type='imb')
+    si_forecaster = load_forecaster(dict=dict_pred, type='imb')
 
     tic = time.time()
-    df_past = get_dataframe(list_data=dict_pred['data_past'],steps=dict_pred['lookback'],timeframe='past')
-    df_past_scaled = scaling.scale_data(df_past.drop(['datetime'],axis=1))
-    df_fut = get_dataframe(list_data=dict_pred['data_fut'],steps=dict_pred['lookahead'],timeframe='fut')
-    df_past_scaled = scaling.scale_data(df_fut.drop(['datetime'],axis=1))
-    df_temporal_past = fdp.get_temporal_information(dict_pred,copy.deepcopy(df_past))
-    df_temporal_fut = fdp.get_temporal_information(dict_pred,copy.deepcopy(df_fut))
+    df_past = get_dataframe(list_data=dict_pred['data_past'], steps=dict_pred['lookback'], timeframe='past')
+    df_past_scaled = scaling.scale_data(df_past.drop(['datetime'], axis=1))
+    df_fut = get_dataframe(list_data=dict_pred['data_fut'], steps=dict_pred['lookahead'], timeframe='fut')
+    df_past_scaled = scaling.scale_data(df_fut.drop(['datetime'], axis=1))
+    df_temporal_past = fdp.get_temporal_information(dict_pred, copy.deepcopy(df_past))
+    df_temporal_fut = fdp.get_temporal_information(dict_pred, copy.deepcopy(df_fut))
 
-    data_load_time = time.time()-tic
+    data_load_time = time.time() - tic
 
-    #TODO: add scaling
-
+    # TODO: add scaling
 
     fut_tensor = convert_df_tensor(df_fut)
     past_tensor = convert_df_tensor(df_past)
     past_temp_tensor = convert_df_tensor(df_temporal_past)
     fut_temp_tensor = convert_df_tensor(df_temporal_fut)
-    past_tensor_temp = torch.cat((past_tensor,past_temp_tensor),dim=2)
-    fut_tensor_temp = torch.cat((fut_tensor,fut_temp_tensor),dim=2)
+    past_tensor_temp = torch.cat((past_tensor, past_temp_tensor), dim=2)
+    fut_tensor_temp = torch.cat((fut_tensor, fut_temp_tensor), dim=2)
+
+    SI_FC = si_forecaster([past_tensor_temp, fut_tensor_temp]).cpu().detach().numpy()
+
+    return SI_FC
 
 
-    SI_FC = si_forecaster([past_tensor_temp,fut_tensor_temp]).cpu().detach().numpy()
+if __name__ == '__main__':
+
+    print('hello world')
 
     #SI_FC = si_forecaster([past_tensor,fut_tensor]).detach().numpy()
 
 
-    x=1
 
 
