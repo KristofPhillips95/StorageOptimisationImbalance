0bca30264e16d2d550a83290ae05708588f97028
KristofPhillips95 <kristofphillips@gmail.com>
2023-11-11 15:47:53 +0100

    ad push time to API

--- Scripts/TimeScheduler.py
+++ Scripts/TimeScheduler.py
@@ -1,9 +1,9 @@
 # !/usr/bin/env python3
 import sched, time
-from TimeWriter import write_time_and_reschedule
+from TimeWriter import write_time_API_and_reschedule
 
-interval = 60
+interval = 10
 my_scheduler = sched.scheduler(time.time, time.sleep)
-my_scheduler.enter(interval, 1, write_time_and_reschedule, (my_scheduler,interval,))
+my_scheduler.enter(interval, 1, write_time_API_and_reschedule, (my_scheduler,interval,))
 my_scheduler.run()
 
