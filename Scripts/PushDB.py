e5be812284b2fe05b738e3420994ecc3e3c29018
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-12-03 11:19:41 +0100

    Exception/error catching to keep scheduler running in case of problems

--- Scripts/PushDB.py
+++ Scripts/PushDB.py
@@ -7,37 +7,43 @@ from predict_imb_price import pred_SI
 def write_item_API_and_reschedule(scheduler, interval=60,index=0):
     # Schedule the next call first
     index += 1
-    scheduler.enter(interval, index, write_item_API_and_reschedule, (scheduler, interval,index,))
+    scheduler.enter(interval, 1, write_item_API_and_reschedule, (scheduler, interval,index,))
     # Then do your stuff
     write_item_API(index)
 
 
 def write_item_API(index):
     print(index)
-    SI_FC, last_si, quantiles = pred_SI(dev='cpu')
+    try:
+        SI_FC, last_si, quantiles = pred_SI(dev='cpu')
+        prices_fc_spread = dict()
+        last_si_time = last_si[1]
+        fc_times = [(last_si_time + datetime.timedelta(minutes=15 * fc_step)).strftime("%H:%M:%S") for fc_step in range(SI_FC.shape[0])]
+        print(SI_FC.shape)
+        writing_time = datetime.datetime.now().strftime('%H:%M:%S')
+        for i, fc_time in enumerate(fc_times):
+            prices_fc_spread[fc_time] = [str(SI_FC[i, q]) for q in range(SI_FC.shape[1])]
+        data = {
+            "id": index,
+            "time": last_si_time.strftime('%H:%M:%S'),
+            "imba_price": last_si[0],
+            "imba_price_fc": 1,
+            "charge": 1,
+            "soc": 1,
+            "fc_spread": prices_fc_spread,
+            "quantiles": quantiles,
+            "writing time": writing_time
 
-    prices_fc_spread = dict()
-    last_si_time = last_si[1]
-    fc_times = [(last_si_time + datetime.timedelta(minutes=15 * fc_step)).strftime("%H:%M:%S") for fc_step in range(8)]
-
-    for i, fc_time in enumerate(fc_times):
-        prices_fc_spread[fc_time] = [str(SI_FC[i, q]) for q in range(SI_FC.shape[1])]
-    data = {
-        "id": index,
-        "time": last_si_time.strftime('%H:%M:%S'),
-        "imba_price": last_si[0],
-        "imba_price_fc": 1,
-        "charge": 1,
-        "soc": 1,
-        "fc_spread": prices_fc_spread,
-        "quantiles": quantiles
-    }
+        }
 
 
-    print(f"Writing now to API:", last_si_time)
+        print(f"Writing now to API:", writing_time)
+        response = requests.put("https://swdd9r1vei.execute-api.eu-north-1.amazonaws.com/items", json=data)
+        print(response.text)
+    except Exception as e:
+        print(f"Not writing to API:",e)
 
-    response = requests.put("https://swdd9r1vei.execute-api.eu-north-1.amazonaws.com/items", json=data)
-    print(response.text)
+
 
 
     # filename = os.path.join("..", "time_file.csv")
--- Scripts/PushDB_scheduler.py
+++ Scripts/PushDB_scheduler.py
@@ -1,7 +1,8 @@
 import sched, time
 from PushDB import write_item_API_and_reschedule
 
-interval = 60
+interval = 30*15
+start_index = 1
 my_scheduler = sched.scheduler(time.time, time.sleep)
-my_scheduler.enter(interval, 25, write_item_API_and_reschedule, (my_scheduler,interval,))
+my_scheduler.enter(interval, 1, write_item_API_and_reschedule, (my_scheduler,interval,start_index))
 my_scheduler.run()
