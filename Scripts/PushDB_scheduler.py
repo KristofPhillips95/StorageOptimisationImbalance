43bb204caae32bed7e6bbf9358d4a1695c8ba210
Kristof Phillips <Kristof.phillips@kuleuven.be>
2023-12-03 11:38:52 +0100

    initial interval 0

--- Scripts/PushDB_scheduler.py
+++ Scripts/PushDB_scheduler.py
@@ -4,5 +4,5 @@ from PushDB import write_item_API_and_reschedule
 interval = 30*15
 start_index = 1
 my_scheduler = sched.scheduler(time.time, time.sleep)
-my_scheduler.enter(interval, 1, write_item_API_and_reschedule, (my_scheduler,interval,start_index))
+my_scheduler.enter(0, 1, write_item_API_and_reschedule, (my_scheduler,interval,start_index))
 my_scheduler.run()
